de3b8e3ad69f0d00d04177802517e9020dc77f8d
AndreyGesler <gesler.a@gmail.com>
2022-11-06 18:18:50 +0600

    is_exists? added to state_utils.ex

--- lib/utils/state_utils2.ex
+++ lib/utils/state_utils2.ex
@@ -1,11 +1,71 @@
-defmodule StateUtils do
+defmodule StateUtils2 do
   ##############################################################################
   ##############################################################################
   @moduledoc """
 
   """
 
-  use Utils
+  use GenServer
+
+  import Macros
+
+  require Logger
+  require Macros
+
+  alias __MODULE__, as: SelfModule
+
+  @supervisor_name StateUtils.Supervisor
+
+  ##############################################################################
+  @doc """
+
+  """
+  @impl true
+  def init(state) do
+    name = Map.fetch!(state, :name)
+    Registry.register(AP.Registry, name, :value)
+
+    {:ok, state}
+  end
+
+  ##############################################################################
+  @doc """
+
+  """
+  def start_link(state \\ %{}) do
+    name = Map.fetch!(state, :name)
+
+    GenServer.start_link(SelfModule, state, name: name)
+  end
+
+  ##############################################################################
+  @doc """
+
+  """
+  @impl true
+  def handle_call({:get, key}, _from, state) do
+    value = Map.get(state, key)
+
+    {:reply, {:ok, value}, state}
+  end
+
+  @impl true
+  def handle_call(:get, _from, state) do
+    {:reply, {:ok, state}, state}
+  end
+
+  @impl true
+  def handle_call({:set, state}, _from, old_state) do
+    {:reply, {:ok, old_state}, state}
+  end
+
+  @impl true
+  def handle_call({:set, key, value}, _from, state) do
+    old_value = Map.get(state, key)
+    state = Map.put(state, key, value)
+
+    {:reply, {:ok, old_value}, state}
+  end
 
   ##############################################################################
   @doc """
@@ -15,12 +75,36 @@ defmodule StateUtils do
 
   def init_state!(name, state)
       when not is_atom(name) or not is_map(state),
-      do: Macros.throw_error!(:CODE_WRONG_FUNCTION_ARGUMENT_ERROR, ["name, state cannot be nil; name must be an atom; state must a map"])
+      do:
+        throw_error!(:CODE_WRONG_FUNCTION_ARGUMENT_ERROR, [
+          "name, state cannot be nil; name must be an atom; state must a map"
+        ])
 
   def init_state!(name, state) do
     result =
       catch_error!(
-        Agent.start_link(fn -> state end, name: name),
+        (
+          opts = [
+            strategy: :one_for_one,
+            name: @supervisor_name
+          ]
+
+          state = Map.put(state, :name, name)
+          item = Supervisor.child_spec({SelfModule, state}, id: name)
+
+          result = Supervisor.start_link([item], opts)
+
+          result =
+            case result do
+              {:error, {:already_started, _}} ->
+                Supervisor.start_child(@supervisor_name, item)
+
+              _ ->
+                result
+            end
+
+          result
+        ),
         false
       )
 
@@ -64,12 +148,15 @@ defmodule StateUtils do
   """
   def get_state!(name)
       when not is_atom(name),
-      do: Macros.throw_error!(:CODE_WRONG_FUNCTION_ARGUMENT_ERROR, ["name cannot be nil; name must be an atom"])
+      do:
+        throw_error!(:CODE_WRONG_FUNCTION_ARGUMENT_ERROR, [
+          "name cannot be nil; name must be an atom"
+        ])
 
   def get_state!(name) do
     result =
       catch_error!(
-        Agent.get(name, fn state -> state end),
+        GenServer.call(name, :get),
         false
       )
 
@@ -93,7 +180,7 @@ defmodule StateUtils do
           result
       end
 
-    {:ok, result}
+    result
   end
 
   ##############################################################################
@@ -102,12 +189,15 @@ defmodule StateUtils do
   """
   def get_state!(name, key)
       when not is_atom(name) or (not is_atom(key) and not is_bitstring(key)),
-      do: Macros.throw_error!(:CODE_WRONG_FUNCTION_ARGUMENT_ERROR, ["name, key cannot be nil; name must be an atom; key must a string or an atom"])
+      do:
+        throw_error!(:CODE_WRONG_FUNCTION_ARGUMENT_ERROR, [
+          "name, key cannot be nil; name must be an atom; key must a string or an atom"
+        ])
 
   def get_state!(name, key) do
     result =
       catch_error!(
-        Agent.get(name, &Map.get(&1, key)),
+        GenServer.call(name, {:get, key}),
         false
       )
 
@@ -131,7 +221,7 @@ defmodule StateUtils do
           result
       end
 
-    {:ok, result}
+    result
   end
 
   ##############################################################################
@@ -140,19 +230,22 @@ defmodule StateUtils do
   """
   def set_state!(name, state)
       when not is_atom(name) or not is_map(state),
-      do: Macros.throw_error!(:CODE_WRONG_FUNCTION_ARGUMENT_ERROR, ["name, state cannot be nil; name must be an atom; state must a map"])
+      do:
+        throw_error!(:CODE_WRONG_FUNCTION_ARGUMENT_ERROR, [
+          "name, state cannot be nil; name must be an atom; state must a map"
+        ])
 
   def set_state!(name, state) do
     result =
       catch_error!(
-        Agent.cast(name, fn _state -> state end),
+        GenServer.call(name, {:set, state}),
         false
       )
 
     result =
       case result do
-        :ok ->
-          :ok
+        {:ok, _} ->
+          result
 
         {:error, _code, %{reason: reason} = _data, _messages} ->
           throw_error!(
@@ -186,19 +279,22 @@ defmodule StateUtils do
   """
   def set_state!(name, key, value)
       when not is_atom(name) or (not is_atom(key) and not is_bitstring(key)),
-      do: Macros.throw_error!(:CODE_WRONG_FUNCTION_ARGUMENT_ERROR, ["name, key, state cannot be nil; name must be an atom; key must an atom or a string"])
+      do:
+        throw_error!(:CODE_WRONG_FUNCTION_ARGUMENT_ERROR, [
+          "name, key, state cannot be nil; name must be an atom; key must an atom or a string"
+        ])
 
   def set_state!(name, key, value) do
     result =
       catch_error!(
-        Agent.cast(name, &Map.put(&1, key, value)),
+        GenServer.call(name, {:set, key, value}),
         false
       )
 
     result =
       case result do
-        :ok ->
-          :ok
+        {:ok, _} ->
+          result
 
         {:error, _code, %{reason: reason} = _data, _messages} ->
           throw_error!(
@@ -232,7 +328,10 @@ defmodule StateUtils do
   """
   def is_exists?(name)
       when not is_atom(name),
-      do: throw_error!(:CODE_WRONG_FUNCTION_ARGUMENT_ERROR, ["name cannot be nil; name must be an atom"])
+      do:
+        throw_error!(:CODE_WRONG_FUNCTION_ARGUMENT_ERROR, [
+          "name cannot be nil; name must be an atom"
+        ])
 
   def is_exists?(name) do
     result =
@@ -255,6 +354,33 @@ defmodule StateUtils do
       end
 
     result
+  end
+
+  ##############################################################################
+  @doc """
+
+  """
+  def string_to_atom(val)
+      when not is_bitstring(val) and not is_atom(val),
+      do: build_error_(:CODE_WRONG_FUNCTION_ARGUMENT_ERROR, ["val must be an atom or string"])
+
+  def string_to_atom(val) when is_atom(val) do
+    {:ok, val}
+  end
+
+  def string_to_atom(val) when is_nil(val) do
+    {:ok, nil}
+  end
+
+  def string_to_atom(val) when is_bitstring(val) do
+    result =
+      try do
+        String.to_existing_atom(val)
+      rescue
+        _ -> String.to_atom(val)
+      end
+
+    {:ok, result}
   end
 
   ##############################################################################
